e6722db3e68ae694beb396bd456b7c230e12b841
Ghonim88 <a.ghonim046@gmail.com>
2025-10-27 11:08:40 +0100

    final check

--- Models/JobResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace WeatherImageFunction.Models;
-
-public class JobResponse
-{
-    public required string JobId { get; set; }
-    public required string Status { get; set; }
-    public string? Message { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}   
